b3f94d435d45398037707ca26955a679a836004c
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-02-16 18:34:39 +0100

    update

--- data/script.js
+++ data/script.js
@@ -880,6 +880,26 @@ movies.map((movie) => {
 // 		console.log(err);
 // 	});
 
+// another example of promises
+
+// const delayedColorChange = (color, delay) => {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			document.body.style.backgroundColor = color;
+// 			resolve();
+// 		}, delay);
+// 	});
+// };
+
+// delayedColorChange("red", 1000)
+// 	.then(() => delayedColorChange("orange", 1000))
+// 	.then(() => delayedColorChange("green", 1000))
+// 	.then(() => delayedColorChange("blue", 1000))
+// 	.then(() => delayedColorChange("pink", 1000))
+// 	.then(() => delayedColorChange("indigo", 1000));
+
+//
+//
 //
 //
 //
